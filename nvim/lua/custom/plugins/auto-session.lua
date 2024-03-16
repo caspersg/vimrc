292f1a1e6f345bc2926fd3d42d50c9919e6bf77d
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-03-16 13:10:12 +1100

    fix NvimTree dir behaviour with auto-session

--- nvim/lua/custom/plugins/auto-session.lua
+++ nvim/lua/custom/plugins/auto-session.lua
@@ -1,9 +1,19 @@
+local function change_nvim_tree_dir()
+  local nvim_tree = require("nvim-tree")
+  nvim_tree.change_dir(vim.fn.getcwd())
+end
+
 return {
   "rmagatti/auto-session",
   config = function()
     require("auto-session").setup({
+      -- auto_save_enabled = true,
+      -- auto_session_enable_last_session = true,
+      -- auto_session_last_session_dir = "~/.local/share/nvim/sessions",
       log_level = "error",
       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      post_restore_cmds = { change_nvim_tree_dir, "NvimTreeOpen" },
+      pre_save_cmds = { "NvimTreeClose" },
     })
   end,
 }
