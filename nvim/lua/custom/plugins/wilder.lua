cef2c182a046de113725a90e78b85c854f5bbe35
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-04-11 00:13:52 +1000

    feat: thinner wilder

--- nvim/lua/custom/plugins/wilder.lua
+++ nvim/lua/custom/plugins/wilder.lua
@@ -31,7 +31,10 @@ return {
           fuzzy = 1,
           fuzzy_filter = wilder.lua_fzy_filter(),
         }),
-        wilder.vim_search_pipeline()
+        wilder.vim_search_pipeline({
+          fuzzy = 1,
+          fuzzy_filter = wilder.lua_fzy_filter(),
+        })
       ),
     })
     wilder.set_option(
@@ -42,12 +45,14 @@ return {
         border = "rounded",
         max_height = "75%", -- max height of the palette
         min_height = 0, -- set to the same as 'max_height' for a fixed height window
+        min_width = "50%",
+        max_width = "50%",
         prompt_position = "top", -- 'top' or 'bottom' to set the location of the prompt
         reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
 
         highlighter = {
-          wilder.basic_highlighter(),
-          wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
+          -- wilder.basic_highlighter(),
+          -- wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
           wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
           -- at https://github.com/romgrk/fzy-lua-native
         },
