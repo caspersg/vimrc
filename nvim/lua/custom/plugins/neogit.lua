aa75dbb65bb065645545ba58fb331a64478debc1
Casper Szymiczek-Graley <casper.szy@gmail.com>
2024-04-24 22:11:58 +1000

    feat: change git to leader i

--- nvim/lua/custom/plugins/neogit.lua
+++ nvim/lua/custom/plugins/neogit.lua
@@ -1,22 +1,22 @@
 return {
-  'NeogitOrg/neogit',
+  "NeogitOrg/neogit",
   dependencies = {
-    'nvim-lua/plenary.nvim', -- required
-    'sindrets/diffview.nvim', -- optional - Diff integration
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
 
     -- Only one of these is needed, not both.
-    'nvim-telescope/telescope.nvim', -- optional
-    'ibhagwan/fzf-lua', -- optional
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua", -- optional
   },
   config = function()
-    require('neogit').setup {
+    require("neogit").setup({
       integrations = {
         telescope = true,
         diffview = true,
         fzf_lua = true,
       },
-    }
+    })
 
-    vim.keymap.set('n', '<leader>G', ':Neogit<CR>', { desc = 'neo[G]it' })
+    vim.keymap.set("n", "<leader>i", ":Neogit<CR>", { desc = "neog[i]t" })
   end,
 }
